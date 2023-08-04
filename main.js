b894af946d62a2742bd41fe5c0643d55841e545a
nazikou1 <nazaiymturdibekova@gmail.com>
2023-08-04 16:02:38 +0600

    dfgbnj

---
+++ main.js
@@ -0,0 +1,72 @@
+// let Name = "Nazaiym"
+
+
+// console.log(Name[0])
+// console.log(Name[2])
+// console.log(Name[4])
+// console.log(Name[6])
+
+// let hom = "lorem,"
+// let homa = "ipsum"
+// let homi = "dolor "
+// let homo = "sit!"
+// console.log(hom + homa + homi + homo)
+
+//  let a = prompt('Введите свой возраст')
+
+// if (a < 5) {
+//     confirm(" вы малыш!")
+// }
+
+// else if (a < 10) {
+//     confirm('Вы ребенок :)')
+// }
+// else if (a < 18) {
+//     confirm('Вы подросток')
+// }
+
+// else if (a < 35) {
+//     confirm('Вы мужчина')
+// }
+
+// else if (a < 60) {
+//     confirm('Вы пожелой лик')
+// }
+
+// else{
+//     document.write('вы Вампир!')
+// }
+
+
+// function um(){
+
+//     let a = +prompt('')
+//     if(a >= 0 && a <= 9){
+//         alert ('слишком маленький')
+//     }
+//     else if ( a >= 10 && a <= 19){
+//         alert('еще немного')
+//     }
+    
+//     else if ( a >= 20){
+//         alert('вход открыть')
+//     }
+//     else {
+//         alert('ошибка')
+//     }
+// }
+
+
+
+
+// let mim = prompt()
+//   console.log( +mim[1] + +mim[2]+ +mim[0])
+
+// let miu = '234678'
+// if( 10==10){
+    //     console.log('да')
+    // }
+    let num = +prompt("напиши число") 
+ 
+    let god = 2023 - num
+    console.log("вы родились в " + god + " году")
